344b6796e25ee500412d028fabef514cdc5ec3d7
Niranjan Kumar <niranjankumaraina@gmail.com>
2025-01-18 00:47:42 +0530

    added navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,124 @@
-import { School } from "lucide-react";
-import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Separator } from "@/components/ui/separator";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import DarkMode from "@/DarkMode";
+import { Menu, School } from "lucide-react";
 
 const Navbar = () => {
+  const user = true;
+  const role = "instructor";
   return (
     <div className="fixed left-0 right-0 top-0 z-10 h-16 border-b border-b-gray-200 bg-white duration-300 dark:border-b-gray-800 dark:bg-[#0A0A0A]">
       {/* Desktop */}
-      <div className="mx-auto hidden max-w-7xl items-center justify-between md:flex">
-        <School size={"30"} />
-        <h1 className="hidden text-2xl font-extrabold md:block">E-Learning</h1>
+      <div className="mx-auto hidden h-full max-w-7xl items-center justify-between gap-10 md:flex">
+        <div className="flex items-center gap-3">
+          <School size={"30"} />
+          <h1 className="hidden text-2xl font-extrabold md:block">
+            E-Learning
+          </h1>
+        </div>
+        {/*  user icons and dark mode icon */}
+        <div className="flex items-center gap-8">
+          {user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar>
+                  <AvatarImage
+                    src="https://github.com/shadcn.png"
+                    alt="@shadcn"
+                  />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                  <DropdownMenuItem>My learning</DropdownMenuItem>
+                  <DropdownMenuItem>Edit Profile</DropdownMenuItem>
+                  <DropdownMenuItem>Log out</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Dashboard</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <div className="flex items-center gap-4">
+              <Button variant="outline">Login</Button>
+              <Button>Signup</Button>
+            </div>
+          )}
+          <DarkMode />
+        </div>
+      </div>
+
+      {/* Mobile Device */}
+
+      <div className="flex h-full items-center justify-between px-4 md:hidden">
+        <h1 className="text-2xl font-extrabold">E-Learning</h1>
+        <MobileNavbar role={role} />
       </div>
     </div>
   );
 };
 
 export default Navbar;
+
+const MobileNavbar = ({ role }) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          size="icon"
+          className="rounded-full bg-gray-200 hover:bg-gray-200"
+          variant="outline"
+        >
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <SheetHeader className="mt-2 flex flex-row items-center justify-between">
+          <SheetTitle className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            E-Learning
+          </SheetTitle>
+          <DarkMode />
+        </SheetHeader>
+        <Separator className="mr-2" />
+        <nav className="flex flex-col space-y-4">
+          <span>My Learning</span>
+          <span>Edit Profile</span>
+          <p>Log out</p>
+        </nav>
+        {role === "instructor" && (
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Dashboard</Button>
+            </SheetClose>
+          </SheetFooter>
+        )}
+      </SheetContent>
+    </Sheet>
+  );
+};
