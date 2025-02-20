ad691bdcd71a416ef443d127cf56b02edcf05ce2
Niranjan Kumar <niranjankumaraina@gmail.com>
2025-02-20 20:02:54 +0530

    adde course edit route

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -1,4 +1,5 @@
 import { Course } from "../models/course.model.js";
+import { deleteMediaFromCloudinary, uploadMedia } from "../utils/cloudinary.js";
 
 export const createCourse = async (req, res) => {
   try {
@@ -48,6 +49,59 @@ export const getCreatorCourses = async (req, res) => {
     console.log(error);
     return res.status(500).json({
       message: "failed to get all courses",
+    });
+  }
+};
+
+export const editCourse = async (req, res) => {
+  try {
+    const courseId = req.params.courseId;
+    const {
+      courseTitle,
+      subTitle,
+      description,
+      category,
+      courseLevel,
+      coursePrice,
+    } = req.body;
+    const thumbnail = req.file;
+    let course = await Course.findById(courseId);
+    if (!course) {
+      res.status(404).json({ message: "Course not found !" });
+    }
+
+    let courseThumbnail;
+    if (thumbnail) {
+      if (course.courseThumbnail) {
+        const publicId = course.courseThumbnail.split("/").pop().split(".")[0];
+        await deleteMediaFromCloudinary(publicId);
+      }
+
+      // upload thumbnail on cloudinary
+      courseThumbnail = await uploadMedia(thumbnail.path);
+    }
+
+    const updateData = {
+      courseTitle,
+      subTitle,
+      description,
+      category,
+      courseLevel,
+      coursePrice,
+      courseThumbnail: courseThumbnail?.secure_url,
+    };
+    course = await Course.findByIdAndUpdate(courseId, updateData, {
+      new: true,
+    });
+
+    return res.status(200).json({
+      message: "Course updated successfully",
+      course,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Failed to edit course",
     });
   }
 };
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,13 +1,18 @@
 import express from "express";
 import {
   createCourse,
+  editCourse,
   getCreatorCourses,
 } from "../controllers/course.controller.js";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import upload from "../utils/multer.js";
 
 const router = express.Router();
 
 router.route("/").post(isAuthenticated, createCourse);
 router.route("/").get(isAuthenticated, getCreatorCourses);
+router
+  .route("/:courseId")
+  .put(isAuthenticated, upload.single("courseThumbnail"), editCourse);
 
 export default router;
