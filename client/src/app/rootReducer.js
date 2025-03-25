e850c63be7c7d60dd2295a59cd006332639ee619
Niranjan Kumar <niranjankumaraina@gmail.com>
2025-03-25 15:59:39 +0530

    added: purchase api

--- client/src/app/rootReducer.js
+++ client/src/app/rootReducer.js
@@ -2,10 +2,12 @@ import { combineSlices } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import { authApi } from "@/features/api/authApi";
 import { courseApi } from "@/features/api/courseApi";
+import { purchaseApi } from "@/features/api/purchaseApi";
 
 const rootReducer = combineSlices({
   [authApi.reducerPath]: authApi.reducer,
   [courseApi.reducerPath]: courseApi.reducer,
+  [purchaseApi.reducerPath]: purchaseApi.reducer,
   auth: authReducer,
 });
 
